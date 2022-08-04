f5a8f912f993b177785a50a526a89673c861fdcd
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-08-05 08:54:49 +0900

    add logging setting

--- gt_back/gt_back/settings.py
+++ gt_back/gt_back/settings.py
@@ -137,3 +137,72 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging
+#  https://docs.djangoproject.com/en/2.0/topics/logging/
+LOGGING = {
+    "version": 1,
+    "desable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "logging.Formatter",
+            "format": "[%(asctime)s] [%(levelname)s] [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "django.log",
+            "maxBytes": 50 * 1000 * 1000,
+            "backupCount": 1,
+            "formatter": "default",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "django.db.backends": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "django.utils": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "gt_back": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "release": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "tickets": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "user_relations": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "user_settings": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+        "users": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+        },
+    },
+}
