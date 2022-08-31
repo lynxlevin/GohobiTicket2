c0923af10acecd8310bf0869177ede98badcd70d
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-08-31 11:53:05 +0900

    test_destroy_ticket

--- gt_back/tickets/tests/test_ticket_views.py
+++ gt_back/tickets/tests/test_ticket_views.py
@@ -3,7 +3,7 @@ from unittest import mock
 from datetime import date, datetime
 from django.test import Client, TestCase
 from gt_back.messages import ErrorMessages
-from rest_framework import status
+from rest_framework import status, exceptions
 
 from tickets.models import Ticket
 from tickets.test_utils.test_seeds import TestSeed
@@ -176,7 +176,7 @@ class TestTicketViews(TestCase):
                     self.assertNotEqual(
                         params["ticket"]["description"], condition["ticket"].description)
 
-    def test_destroy(self):
+    def test_destroy_integration(self):
         """
         Delete /tickets/{ticket_id}/
         """
@@ -187,69 +187,36 @@ class TestTicketViews(TestCase):
         client = Client()
         client.force_login(user)
 
-        query = Ticket.objects.filter_eq_user_relation_id(giving_relation.id)
-        ticket = query.first()
-        original_ticket_count = query.count()
+        ticket = Ticket.objects.filter_eq_user_relation_id(
+            giving_relation.id).first()
 
         response = client.delete(f"/tickets/{ticket.id}/")
 
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
 
-        self.assertIsNone(Ticket.objects.get_by_id(ticket.id))
-        pro_execution_ticket_count = query.count()
-        self.assertEqual(original_ticket_count - 1, pro_execution_ticket_count)
-
-    def test_destroy_case_error(self):
+    @mock.patch("tickets.use_cases.destroy_ticket.DestroyTicket.execute")
+    def test_destroy_case_error(self, use_case_mock):
         """
         Delete /tickets/{ticket_id}/
         error case
         """
+        test_log = "test_exception_log"
+        use_case_mock.side_effect = exceptions.APIException(detail=test_log)
 
         user = self.seeds.users[1]
 
-        receiving_relation_id = user.receiving_relations.first().id
-        receiving_ticket_id = Ticket.objects.filter_eq_user_relation_id(
-            receiving_relation_id).first().id
-
-        unrelated_relation_id = self.seeds.user_relations[2].id
-        unrelated_ticket_id = Ticket.objects.filter_eq_user_relation_id(
-            unrelated_relation_id).first().id
-
-        giving_relation_id = user.giving_relations.first().id
-        used_ticket = Ticket.objects.filter_eq_user_relation_id(
-            giving_relation_id).first()
-        used_ticket.use_date = date.today()
-        used_ticket.save()
-
-        cases = {
-            "receiving_relation": {"ticket_id": receiving_ticket_id, "status_code": status.HTTP_403_FORBIDDEN, "log": "Only the giving user may delete ticket."},
-            "unrelated_relation": {"ticket_id": unrelated_ticket_id, "status_code": status.HTTP_403_FORBIDDEN, "log": "Only the giving user may delete ticket."},
-            "non_existent_ticket": {"ticket_id": "-1", "status_code": status.HTTP_404_NOT_FOUND, "log": "Ticket not found."},
-            "used_ticket": {"ticket_id": used_ticket.id, "status_code": status.HTTP_403_FORBIDDEN, "log": "Used ticket cannot be deleted."},
-        }
+        ticket_id = "1"
 
         client = Client()
         client.force_login(user)
 
         logger = logging.getLogger("gt_back.exception_handler")
 
-        for case, condition in cases.items():
-            with self.subTest(case=case):
-                with self.assertLogs(logger=logger, level=logging.WARN) as cm:
-                    response = client.delete(
-                        f"/tickets/{condition['ticket_id']}/")
+        with self.assertLogs(logger=logger, level=logging.WARN) as cm:
+            client.delete(f"/tickets/{ticket_id}/")
 
-                self.assertEqual(
-                    condition["status_code"], response.status_code)
-
-                expected_log = [
-                    f"WARNING:gt_back.exception_handler:DestroyTicket_exception: {condition['log']}"]
-                self.assertEqual(expected_log, cm.output)
-
-                if not case in ["non_existent_ticket"]:
-                    self.assertIsNotNone(
-                        Ticket.objects.get_by_id(condition["ticket_id"]))
-                # MYMEMO: add use_case test
+        expected_log = [f"WARNING:gt_back.exception_handler:{test_log}"]
+        self.assertEqual(expected_log, cm.output)
 
     def test_mark_special(self):
         """
---
+++ gt_back/tickets/tests/use_cases/test_destroy_ticket.py
@@ -0,0 +1,72 @@
+from datetime import date
+import logging
+
+from django.test import TestCase
+from rest_framework import exceptions
+
+from tickets.models import Ticket
+from tickets.use_cases import DestroyTicket
+from tickets.test_utils.test_seeds import TestSeed
+
+
+class TestDestroyTicket(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.seeds = TestSeed()
+        cls.seeds.setUp()
+
+    def test_execute(self):
+        user = self.seeds.users[1]
+        giving_relation = user.giving_relations.first()
+
+        query = Ticket.objects.filter_eq_user_relation_id(giving_relation.id)
+        ticket = query.first()
+        original_ticket_count = query.count()
+
+        class_name = "tickets.use_cases.destroy_ticket"
+        logger = logging.getLogger(class_name)
+
+        with self.assertLogs(logger=logger, level=logging.INFO) as cm:
+            DestroyTicket().execute(ticket_id=ticket.id, user=user)
+
+        self.assertIsNone(Ticket.objects.get_by_id(ticket.id))
+        pro_execution_ticket_count = query.count()
+        self.assertEqual(original_ticket_count - 1, pro_execution_ticket_count)
+
+        expected_log = [f"INFO:{class_name}:DestroyTicket"]
+        self.assertEqual(expected_log, cm.output)
+
+    def test_execute_case_error(self):
+        user = self.seeds.users[1]
+
+        receiving_relation_id = user.receiving_relations.first().id
+        receiving_ticket_id = Ticket.objects.filter_eq_user_relation_id(
+            receiving_relation_id).first().id
+
+        unrelated_relation_id = self.seeds.user_relations[2].id
+        unrelated_ticket_id = Ticket.objects.filter_eq_user_relation_id(
+            unrelated_relation_id).first().id
+
+        giving_relation_id = user.giving_relations.first().id
+        used_ticket = Ticket.objects.filter_eq_user_relation_id(
+            giving_relation_id).first()
+        used_ticket.use_date = date.today()
+        used_ticket.save()
+
+        cases = {
+            "receiving_relation": {"ticket_id": receiving_ticket_id, "exception": exceptions.PermissionDenied, "detail": "Only the giving user may delete ticket."},
+            "unrelated_relation": {"ticket_id": unrelated_ticket_id, "exception": exceptions.PermissionDenied, "detail": "Only the giving user may delete ticket."},
+            "non_existent_ticket": {"ticket_id": "-1", "exception": exceptions.NotFound, "detail": "Ticket not found."},
+            "used_ticket": {"ticket_id": used_ticket.id, "exception": exceptions.PermissionDenied, "detail": "Used ticket cannot be deleted."},
+        }
+
+        for case, condition in cases.items():
+            with self.subTest(case=case):
+                expected_exc_detail = f"DestroyTicket_exception: {condition['detail']}"
+                with self.assertRaisesRegex(condition["exception"], expected_exc_detail):
+                    DestroyTicket().execute(
+                        ticket_id=condition["ticket_id"], user=user)
+
+                if not case in ["non_existent_ticket"]:
+                    self.assertIsNotNone(
+                        Ticket.objects.get_by_id(condition["ticket_id"]))
