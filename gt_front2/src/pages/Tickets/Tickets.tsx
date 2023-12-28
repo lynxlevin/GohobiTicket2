7f13e1ba1a44eaf0892704c42a1c0ebd8818d62b
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2023-12-28 10:42:00 +0900

    Tickets page: titles and form

--- gt_front2/src/pages/Tickets/Tickets.tsx
+++ gt_front2/src/pages/Tickets/Tickets.tsx
@@ -6,7 +6,10 @@ import {
     CardContent,
     CardMedia,
     Grid,
-    Stack,
+    FormGroup,
+    FormControlLabel,
+    Checkbox,
+    TextField,
     Box,
     Typography,
     Container,
@@ -16,6 +19,7 @@ import TicketAppBar from './TicketsAppBar';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+// Copied template from https://github.com/mui/material-ui/tree/v5.15.2/docs/data/material/getting-started/templates/album
 const Tickets = () => {
     return (
         <>
@@ -29,82 +33,83 @@ const Tickets = () => {
                 pb: 6,
             }}
             >
-            <Container maxWidth="sm">
-                <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-                gutterBottom
-                >
-                Album layout
-                </Typography>
-                <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                Something short and leading about the collection below—its contents,
-                the creator, etc. Make it short and sweet, but not too short so folks
-                don&apos;t simply skip over it entirely.
-                </Typography>
-                <Stack
-                sx={{ pt: 4 }}
-                direction="row"
-                spacing={2}
-                justifyContent="center"
-                >
-                <Button variant="contained">Main call to action</Button>
-                <Button variant="outlined">Secondary action</Button>
-                </Stack>
-            </Container>
-            </Box>
-            <Container sx={{ py: 8 }} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-                {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Card
-                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                <Container maxWidth="sm">
+                    <Typography
+                    variant="h5"
+                    align="center"
+                    color="text.primary"
+                    sx={{ mt: 3 }}
+                    gutterBottom
                     >
-                    <CardMedia
-                        component="div"
-                        sx={{
-                        // 16:9
-                        pt: '56.25%',
-                        }}
-                        image="https://source.unsplash.com/random?wallpapers"
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        Heading
-                        </Typography>
-                        <Typography>
-                        This is a media card. You can use this section to describe the
-                        content.
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">View</Button>
-                        <Button size="small">Edit</Button>
-                    </CardActions>
-                    </Card>
+                    ちひろにあげる
+                    </Typography>
+                    <Typography
+                    variant="h4"
+                    align="center"
+                    color="text.primary"
+                    sx={{ fontWeight: 600 }}
+                    gutterBottom
+                    >
+                    ごほうびチケット
+                    </Typography>
+                    <CardMedia sx={{ pt: '60%', backgroundSize: 'contain' }} component="div" image="/IMG_5777.jpeg" />
+                    {/* <CardMedia sx={{ pt: '60%', backgroundSize: 'contain' }} component="div" image="/IMG_5928.jpeg" /> */}
+                    <Typography
+                    variant="h5"
+                    align="center"
+                    color="text.primary"
+                    gutterBottom
+                    >
+                        計747枚
+                    </Typography>
+                    <FormGroup>
+                        <FormControlLabel label="特別チケットのみ表示" control={<Checkbox />} />
+                        <FormControlLabel label="使用済みチケットのみ表示" control={<Checkbox />} />
+                    </FormGroup>
+                    <FormGroup>
+
+                        <FormControlLabel label="特別チケットにする" control={<Checkbox />} />
+                        <TextField label="内容" multiline minRows={5}/>
+                        <Button variant="contained">チケット付与</Button>
+                        <Button variant="outlined" sx={{color: 'primary.dark'}}>下書き保存</Button>
+                    </FormGroup>
+                </Container>
+            </Box>
+            {/* End hero unit */}
+            <Container sx={{ py: 8 }} maxWidth="md">
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                    <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                        >
+                        <CardMedia
+                            component="div"
+                            sx={{
+                            // 16:9
+                            pt: '56.25%',
+                            }}
+                            image="https://source.unsplash.com/random?wallpapers"
+                        />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                            Heading
+                            </Typography>
+                            <Typography>
+                            This is a media card. You can use this section to describe the
+                            content.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View</Button>
+                            <Button size="small">Edit</Button>
+                        </CardActions>
+                        </Card>
+                    </Grid>
+                    ))}
                 </Grid>
-                ))}
-            </Grid>
             </Container>
         </main>
-        {/* Footer */}
-        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-            <Typography variant="h6" align="center" gutterBottom>
-            Footer
-            </Typography>
-            <Typography
-            variant="subtitle1"
-            align="center"
-            color="text.secondary"
-            component="p"
-            >
-            Something here to give the footer a purpose!
-            </Typography>
-        </Box>
-        {/* End footer */}
         </>
     );
 }
