4c48f508ac77d0df30c7cb4063483ca2649f82bf
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-01-01 16:31:24 +0900

    Ticket: Improve performance

--- gt_front2/src/pages/Tickets/Ticket.tsx
+++ gt_front2/src/pages/Tickets/Ticket.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import {
     Badge,
     Button,
@@ -17,21 +17,35 @@ const Ticket = (props: any) => {
     const { ticket, isUsed } = props;
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
+    const getStatusBadge = useMemo(() => {
+        let text;
+        switch (ticket.status) {
+            case 'unread':
+                text = 'NEW!!';
+                break;
+            case 'edited':
+                text = 'EDITED!!';
+                break;
+            case 'draft':
+                text = 'DRAFT';
+                break;
+        }
+        return <Badge color='primary' badgeContent={text} sx={{display: 'block', mr: 3.5}} />
+    }, [ticket.status])
+
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Badge color='primary' badgeContent='NEW!!' sx={{display: 'block', mr: 3.5}} />
+            {ticket.status !== 'read' && getStatusBadge}
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', position: 'relative' }}>
                 <CardContent sx={{ flexGrow: 1 }}>
-                    <Grid container>
-                        <Grid item xs={12} sx={{ position: 'relative' }}>
-                            <Typography gutterBottom variant="subtitle1" sx={{pt: 1, pb: 1}}>
-                                {format(new Date(ticket.gift_date), 'yyyy-MM-dd E')}
-                            </Typography>
-                            {!isUsed && (
-                                <IconButton sx={{ position: 'absolute', top: '-5px', right: '-4px' }} onClick={() => setIsEditModalOpen(true)} size='small'><EditIcon /></IconButton>
-                            )}
-                        </Grid>
-                    </Grid>
+                    <div style={{position: 'relative'}}>
+                        <Typography gutterBottom variant="subtitle1" sx={{pt: 1, pb: 1}}>
+                            {format(new Date(ticket.gift_date), 'yyyy-MM-dd E')}
+                        </Typography>
+                        {!isUsed && (
+                            <IconButton sx={{ position: 'absolute', top: '-5px', right: '-4px' }} onClick={() => setIsEditModalOpen(true)} size='small'><EditIcon /></IconButton>
+                        )}
+                    </div>
                     <Typography sx={{whiteSpace: 'pre-wrap', mb: 2}}>
                         {ticket.description}
                     </Typography>
