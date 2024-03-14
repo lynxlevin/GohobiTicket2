64403384e425c634d0b0d5ec6d9d385853e74d57
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-03-14 20:58:23 +0900

    Add HideOnScroll to DiariesAppBar

--- gt_front2/src/pages/Diaries/DiariesAppBar.tsx
+++ gt_front2/src/pages/Diaries/DiariesAppBar.tsx
@@ -1,9 +1,26 @@
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
 import SellIcon from '@mui/icons-material/Sell';
-import { AppBar, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import { AppBar, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Slide, Toolbar, useScrollTrigger } from '@mui/material';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+interface HideOnScrollProps {
+    children: React.ReactElement;
+}
+
+const HideOnScroll = (props: HideOnScrollProps) => {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+        target: window,
+    });
+
+    return (
+        <Slide appear={false} direction='down' in={!trigger}>
+            {children}
+        </Slide>
+    );
+};
 
 interface DiariesAppBarProps {
     handleLogout: () => Promise<void>;
@@ -17,40 +34,42 @@ const DiariesAppBar = (props: DiariesAppBarProps) => {
     const navigate = useNavigate();
 
     return (
-        <AppBar position='fixed' sx={{ bgcolor: 'primary.light' }}>
-            <Toolbar>
-                <div style={{ flexGrow: 1 }} />
-                <IconButton onClick={() => setTopBarDrawerOpen(true)}>
-                    <MenuIcon sx={{ color: 'rgba(0,0,0,0.67)' }} />
-                </IconButton>
-                <Drawer anchor='right' open={topBarDrawerOpen} onClose={() => setTopBarDrawerOpen(false)}>
-                    <List>
-                        <ListItem>
-                            <ListItemButton
-                                disableGutters
-                                onClick={() => {
-                                    window.scroll({ top: 0 });
-                                    navigate(`/diary_tags?user_relation_id=${userRelationId}`);
-                                }}
-                            >
-                                <ListItemIcon>
-                                    <SellIcon />
-                                </ListItemIcon>
-                                <ListItemText>タグ編集</ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemButton disableGutters onClick={handleLogout}>
-                                <ListItemIcon>
-                                    <LogoutIcon />
-                                </ListItemIcon>
-                                <ListItemText>ログアウト</ListItemText>
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Drawer>
-            </Toolbar>
-        </AppBar>
+        <HideOnScroll>
+            <AppBar position='fixed' sx={{ bgcolor: 'primary.light' }}>
+                <Toolbar>
+                    <div style={{ flexGrow: 1 }} />
+                    <IconButton onClick={() => setTopBarDrawerOpen(true)}>
+                        <MenuIcon sx={{ color: 'rgba(0,0,0,0.67)' }} />
+                    </IconButton>
+                    <Drawer anchor='right' open={topBarDrawerOpen} onClose={() => setTopBarDrawerOpen(false)}>
+                        <List>
+                            <ListItem>
+                                <ListItemButton
+                                    disableGutters
+                                    onClick={() => {
+                                        window.scroll({ top: 0 });
+                                        navigate(`/diary_tags?user_relation_id=${userRelationId}`);
+                                    }}
+                                >
+                                    <ListItemIcon>
+                                        <SellIcon />
+                                    </ListItemIcon>
+                                    <ListItemText>タグ編集</ListItemText>
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemButton disableGutters onClick={handleLogout}>
+                                    <ListItemIcon>
+                                        <LogoutIcon />
+                                    </ListItemIcon>
+                                    <ListItemText>ログアウト</ListItemText>
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                    </Drawer>
+                </Toolbar>
+            </AppBar>
+        </HideOnScroll>
     );
 };
 export default DiariesAppBar;
