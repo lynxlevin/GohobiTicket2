e852bc10e661cb04fb4751ec823546d6eaf1ea7f
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-03-15 20:48:28 +0900

    Migrate command

---
+++ gt_back/user_relations/management/commands/migrate_user_relation_to_new_model.py
@@ -0,0 +1,45 @@
+from diaries.models import Diary, DiaryTag
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from tickets.models import Ticket
+from user_relations.models import UserRelation, UserRelation2
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        old_relations: list[UserRelation] = UserRelation.objects.all()
+
+        for old in old_relations:
+            existing_new_relations = UserRelation2.objects.filter(
+                Q(user_1=old.giving_user, user_2=old.receiving_user)
+                | Q(user_1=old.receiving_user, user_2=old.giving_user)
+            )
+
+            if existing_new_relations.exists():
+                existing_new_relation: UserRelation2 = existing_new_relations.first()
+                if existing_new_relation.user_2 == old.giving_user:
+                    existing_new_relation.user_2_giving_ticket_img = old.ticket_img
+                else:
+                    existing_new_relation.user_1_giving_ticket_img = old.ticket_img
+                existing_new_relation.save()
+                Ticket.objects.filter_eq_user_relation_id(old.id, use_old=True).update(
+                    user_relation_2=existing_new_relation, giving_user=old.giving_user
+                )
+                Diary.objects.filter_eq_user_relation_id(old.id, use_old=True).update(
+                    user_relation_2=existing_new_relation
+                )
+                DiaryTag.objects.filter_eq_user_relation_id(old.id, use_old=True).update(
+                    user_relation_2=existing_new_relation
+                )
+            else:
+                new_relation = UserRelation2.objects.create(
+                    user_1=old.giving_user,
+                    user_2=old.receiving_user,
+                    user_1_giving_ticket_img=old.ticket_img,
+                    user_2_giving_ticket_img="",
+                )
+                Ticket.objects.filter_eq_user_relation_id(old.id, use_old=True).update(
+                    user_relation_2=new_relation, giving_user=old.giving_user
+                )
+                Diary.objects.filter_eq_user_relation_id(old.id, use_old=True).update(user_relation_2=new_relation)
+                DiaryTag.objects.filter_eq_user_relation_id(old.id, use_old=True).update(user_relation_2=new_relation)
