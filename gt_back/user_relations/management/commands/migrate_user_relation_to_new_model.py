5c4e9ed7a67570f8114623e3ca1bc9b0a379b5e9
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-03-18 21:02:11 +0900

    Update migrate_user_relation_to_new_model command to update default_page

--- gt_back/user_relations/management/commands/migrate_user_relation_to_new_model.py
+++ gt_back/user_relations/management/commands/migrate_user_relation_to_new_model.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from diaries.models import Diary, DiaryTag
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 from tickets.models import Ticket
 from user_relations.models import UserRelation, UserRelationOld
+from user_settings.models import UserSetting
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         old_relations: list[UserRelationOld] = UserRelationOld.objects.all()
+        user_settings: list[UserSetting] = UserSetting.objects.all()
 
         for old in old_relations:
             existing_new_relations = UserRelation.objects.filter(
@@ -31,6 +35,7 @@ class Command(BaseCommand):
                 DiaryTag.objects.filter_eq_user_relation_id(old.id, use_old=True).update(
                     user_relation=existing_new_relation
                 )
+                self.update_user_settings(user_settings, old, existing_new_relation)
             else:
                 new_relation = UserRelation.objects.create(
                     user_1=old.giving_user,
@@ -43,3 +48,11 @@ class Command(BaseCommand):
                 )
                 Diary.objects.filter_eq_user_relation_id(old.id, use_old=True).update(user_relation=new_relation)
                 DiaryTag.objects.filter_eq_user_relation_id(old.id, use_old=True).update(user_relation=new_relation)
+                self.update_user_settings(user_settings, old, new_relation)
+
+    def update_user_settings(self, user_settings, old_relation, new_relation):
+        for setting in user_settings:
+            default_relation_id = Path(setting.default_page).name
+            if default_relation_id == str(old_relation.id):
+                setting.default_page = Path(setting.default_page).with_name(str(new_relation.id)).as_posix()
+                setting.save()
