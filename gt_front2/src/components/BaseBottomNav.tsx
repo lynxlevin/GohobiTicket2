2ba2dd15cee8505dec37f0139fe17b3fda1bb1e9
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-01-19 16:43:33 +0900

    feat: larger max number for badge

--- gt_front2/src/components/BaseBottomNav.tsx
+++ gt_front2/src/components/BaseBottomNav.tsx
@@ -27,7 +27,13 @@ const BaseBottomNav = ({ selectedNavItem, handleSelect, navActions, badges }: Ba
                     value="giving_tickets"
                     label="あげる"
                     icon={
-                        <Badge badgeContent={badges?.givingTickets} color="primary" variant={badges?.givingTickets === 0 ? 'dot' : 'standard'} showZero>
+                        <Badge
+                            badgeContent={badges?.givingTickets}
+                            color="primary"
+                            variant={badges?.givingTickets === 0 ? 'dot' : 'standard'}
+                            showZero
+                            max={999}
+                        >
                             <SwitchAccessShortcutIcon />
                         </Badge>
                     }
@@ -37,7 +43,13 @@ const BaseBottomNav = ({ selectedNavItem, handleSelect, navActions, badges }: Ba
                     value="receiving_tickets"
                     label="もらう"
                     icon={
-                        <Badge badgeContent={badges?.receivingTickets} color="primary" variant={badges?.receivingTickets === 0 ? 'dot' : 'standard'} showZero>
+                        <Badge
+                            badgeContent={badges?.receivingTickets}
+                            color="primary"
+                            variant={badges?.receivingTickets === 0 ? 'dot' : 'standard'}
+                            showZero
+                            max={999}
+                        >
                             <SwitchAccessShortcutIcon style={{ rotate: '180deg' }} />
                         </Badge>
                     }
@@ -47,7 +59,7 @@ const BaseBottomNav = ({ selectedNavItem, handleSelect, navActions, badges }: Ba
                     value="diaries"
                     label="日記"
                     icon={
-                        <Badge badgeContent={badges?.diaries} color="primary" variant={badges?.diaries === 0 ? 'dot' : 'standard'} showZero>
+                        <Badge badgeContent={badges?.diaries} color="primary" variant={badges?.diaries === 0 ? 'dot' : 'standard'} showZero max={999}>
                             <BookIcon />
                         </Badge>
                     }
