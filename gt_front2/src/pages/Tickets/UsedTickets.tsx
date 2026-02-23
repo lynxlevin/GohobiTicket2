11b892c5eae9eb98839fcd223a60ff5418837627
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-02-23 20:52:29 +0900

    feat: ui with thread

--- gt_front2/src/pages/Tickets/UsedTickets.tsx
+++ gt_front2/src/pages/Tickets/UsedTickets.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Button, Card, CardActions, CardContent, Container, Grid, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, Container, Divider, Grid, IconButton, Stack, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import BottomNav from '../../components/BottomNav';
 import useTicketContext from '../../hooks/useTicketContext';
@@ -10,6 +10,8 @@ import CommonAppBar from '../../components/CommonAppBar';
 import { Navigate } from 'react-router-dom';
 import { ITicket } from '../../contexts/ticket-context';
 import { format } from 'date-fns';
+import InfoIcon from '@mui/icons-material/Info';
+import EditIcon from '@mui/icons-material/Edit';
 import SpecialStamp from './SpecialStamp';
 import DetailDialog from './DetailDialog';
 
@@ -86,9 +88,11 @@ const MiniLogo = styled.img`
 interface UsedTicketProps {
     ticket: ITicket;
     relatedUserName: string;
+    hasThreadPosts?: boolean;
+    showAll?: boolean;
 }
 
-const UsedTicket = ({ ticket, relatedUserName }: UsedTicketProps) => {
+const UsedTicket = ({ ticket, relatedUserName, hasThreadPosts = true, showAll = false }: UsedTicketProps) => {
     const [openedDialog, setOpenedDialog] = useState<'Detail'>();
 
     const getDialog = () => {
@@ -100,31 +104,45 @@ const UsedTicket = ({ ticket, relatedUserName }: UsedTicketProps) => {
 
     if (ticket.use_date === null) return <></>;
     return (
-        <StyledGrid item xs={12} sm={6} md={4}>
+        <StyledTicket item xs={12} sm={6} md={4}>
             <Card className="card">
                 <CardContent>
                     {/* MYMEMO: user_id is hardcoded here. */}
-                    <Typography className="ticket-user">{ticket.giving_user_id === 1 ? relatedUserName : '自分'}からのお願い</Typography>
-                    <Typography className="ticket-date">{format(new Date(ticket.use_date), 'yyyy-MM-dd')}</Typography>
-                    <Typography className="ticket-description">{ticket.use_description}</Typography>
+                    <Stack direction="row" justifyContent="space-between">
+                        <Typography className={`from-name${ticket.is_special ? ' special-ticket' : ''}`}>
+                            {ticket.giving_user_id === 1 ? relatedUserName : '自分'}より
+                        </Typography>
+                        <Typography className="post-time">{format(new Date(ticket.use_date), 'yyyy-MM-dd HH:mm')}</Typography>
+                    </Stack>
+                    <Typography className="text">{ticket.use_description}</Typography>
                 </CardContent>
-                <CardActions className="use-button">
-                    <Button variant="outlined" onClick={() => setOpenedDialog('Detail')}>
-                        お礼の内容を見る
-                    </Button>
+                <CardActions className="card-actions">
+                    <IconButton size="small" onClick={() => setOpenedDialog('Detail')}>
+                        <InfoIcon />
+                    </IconButton>
+                    <IconButton size="small">
+                        <EditIcon />
+                    </IconButton>
                 </CardActions>
                 {ticket.is_special && <SpecialStamp randKey={ticket.id} />}
+                {hasThreadPosts && (
+                    <>
+                        <ThreadPost relatedUserName={relatedUserName} showAll={showAll} />
+                        {showAll && <ThreadPost relatedUserName={relatedUserName} showAll={showAll} isLast />}
+                    </>
+                )}
+                {!showAll && (
+                    <Button className="open-thread-button" variant="outlined">
+                        スレッドを開く
+                    </Button>
+                )}
             </Card>
             {openedDialog && getDialog()}
-        </StyledGrid>
+        </StyledTicket>
     );
 };
 
-const StyledGrid = styled(Grid)`
-    .badge {
-        display: block;
-        margin-right: 28px;
-    }
+const StyledTicket = styled(Grid)`
     .card {
         height: 100%;
         display: flex;
@@ -132,33 +150,105 @@ const StyledGrid = styled(Grid)`
         position: relative;
     }
 
-    .relative-div {
-        position: relative;
+    .from-name {
+        text-align: left;
+        font-size: 14px;
+    }
+    .special-ticket {
+        margin-left: 54px;
+        margin-bottom: 16px;
     }
 
-    .ticket-user {
-        text-align: right;
+    .post-time {
+        font-size: 12px;
+        line-height: 21px;
     }
 
-    .ticket-date {
-        text-align: right;
-        padding-bottom: 8px;
+    .text {
+        margin-top: 16px;
+        text-align: start;
+        white-space: pre-wrap;
+        overflow-wrap: anywhere;
+    }
+    .line-clamp {
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        line-clamp: 1;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        white-space: collapse;
+    }
+
+    .card-actions {
+        justify-content: flex-end;
+        margin-top: -16px;
+        margin-bottom: 0px;
+    }
+
+    .open-thread-button {
+        width: 147px;
+        margin-bottom: 16px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+`;
+
+interface ThreadPostProps {
+    relatedUserName: string;
+    showAll?: boolean;
+    isLast?: boolean;
+}
+
+const ThreadPost = ({ relatedUserName, showAll = false, isLast = false }: ThreadPostProps) => {
+    return (
+        <StyledPost>
+            <Divider className="divider" />
+            <Box className="content">
+                <Stack direction="row" justifyContent="space-between">
+                    <Typography className="from-name">{relatedUserName}より</Typography>
+                    <Typography className="post-time">2025-08-18 13:15</Typography>
+                </Stack>
+                <Typography className={`text${showAll ? '' : ' line-clamp'}`}>
+                    あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお
+                </Typography>
+                {showAll && (
+                    <IconButton size="small" className={`edit-button${isLast ? ' is-last' : ''}`}>
+                        <EditIcon />
+                    </IconButton>
+                )}
+                {isLast && (
+                    <Button className="message-button" variant="outlined">
+                        メッセージを送る
+                    </Button>
+                )}
+            </Box>
+        </StyledPost>
+    );
+};
+
+const StyledPost = styled(Box)`
+    position: relative;
+
+    .divider {
+        margin-left: 12px;
+        margin-right: 12px;
+    }
+
+    .content {
+        padding: 16px;
     }
 
     .edit-button {
         position: absolute;
-        top: -8px;
-        right: -7px;
+        bottom: 8px;
+        right: 8px;
+    }
+    .is-last {
+        bottom: 60px;
     }
 
-    .ticket-description {
-        text-align: start;
-        white-space: pre-wrap;
-    }
-
-    .use-button {
-        justify-content: center;
-        margin-bottom: 8px;
+    .message-button {
+        margin-top: 16px;
     }
 `;
 
