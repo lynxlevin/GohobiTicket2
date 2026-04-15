f71d5b07cdafb9f4d1bca6da085b57fae27c386e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-04-15 09:02:50 +0900

    fix: BottomNav from pages without userRelationId in path

--- gt_front2/src/components/BottomNav.tsx
+++ gt_front2/src/components/BottomNav.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import usePagePath from '../hooks/usePagePath';
 import BaseBottomNav, { type NavItem } from './BaseBottomNav';
+import useUserRelationContext from '../hooks/useUserRelationContext';
 
 const BottomNav = () => {
-    const { userRelationId, pagePath } = usePagePath();
+    const { userRelationId: pathUserRelationId, pagePath } = usePagePath();
+    const { userRelations } = useUserRelationContext();
 
     const [selected, setSelected] = useState<NavItem>();
     const handleSelect = (_: React.SyntheticEvent, newValue: NavItem) => {
@@ -12,22 +14,27 @@ const BottomNav = () => {
     };
 
     const navigate = useNavigate();
+    const userRelationId = pathUserRelationId === null ? (userRelations === undefined ? undefined : userRelations[0].id) : pathUserRelationId;
 
     const navActions = {
         giving_tickets: () => {
-            navigate(`/user_relations/${userRelationId}/giving_tickets`);
+            const url = userRelationId !== undefined ? `/user_relations/${pathUserRelationId ?? userRelations![0].id}/giving_tickets` : '/login';
+            navigate(url);
             window.scroll({ top: 0 });
         },
         receiving_tickets: () => {
-            navigate(`/user_relations/${userRelationId}/receiving_tickets`);
+            const url = userRelationId !== undefined ? `/user_relations/${pathUserRelationId ?? userRelations![0].id}/receiving_tickets` : '/login';
+            navigate(url);
             window.scroll({ top: 0 });
         },
         wishes: () => {
-            navigate(`/user_relations/${userRelationId}/wishes`);
+            const url = userRelationId !== undefined ? `/user_relations/${pathUserRelationId ?? userRelations![0].id}/wishes` : '/login';
+            navigate(url);
             window.scroll({ top: 0 });
         },
         diaries: () => {
-            navigate(`/user_relations/${userRelationId}/diaries`);
+            const url = userRelationId !== undefined ? `/user_relations/${pathUserRelationId ?? userRelations![0].id}/diaries` : '/login';
+            navigate(url);
             window.scroll({ top: 0 });
         },
     };
