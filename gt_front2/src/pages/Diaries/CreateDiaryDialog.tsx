daaf83df43ea32c292011ae634140e61fa0a6069
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-05-07 16:10:22 +0900

    feat (CreateDiaryDialog): read/write from/to LocalStorage

--- gt_front2/src/pages/Diaries/CreateDiaryDialog.tsx
+++ gt_front2/src/pages/Diaries/CreateDiaryDialog.tsx
@@ -1,10 +1,11 @@
 import { Box, Button, Chip, Dialog, DialogContent, FormControl, FormGroup, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 import { MobileDatePicker } from '@mui/x-date-pickers';
-import { format } from 'date-fns';
-import { useState } from 'react';
+import { format, parse } from 'date-fns';
+import { ChangeEvent, useEffect, useState } from 'react';
 import useDiaryContext from '../../hooks/useDiaryContext';
 import useDiaryTagContext from '../../hooks/useDiaryTagContext';
 import { IDiaryTag } from '../../types/diary';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 interface CreateDiaryDialogProps {
     userRelationId: number;
@@ -14,6 +15,7 @@ interface CreateDiaryDialogProps {
 const CreateDiaryDialog = ({ userRelationId, onClose }: CreateDiaryDialogProps) => {
     const { createDiary } = useDiaryContext();
     const { diaryTags } = useDiaryTagContext();
+    const { createDiaryDraft, setCreateDiaryDraft, resetCreateDiaryDraft } = useLocalStorage();
 
     const [date, setDate] = useState<Date>(new Date());
     const [tags, setTags] = useState<IDiaryTag[]>([]);
@@ -29,17 +31,52 @@ const CreateDiaryDialog = ({ userRelationId, onClose }: CreateDiaryDialogProps) 
         setDate(new Date());
         setEntry('');
         setTags([]);
+        resetCreateDiaryDraft();
         onClose();
     };
 
     const onChangeDate = (newDate: Date | null) => {
         if (newDate) {
             setDate(newDate);
+            setCreateDiaryDraft({ ...createDiaryDraft, date: format(newDate, 'yyyy-MM-dd') });
         }
     };
+    const onChangeTags = (event: SelectChangeEvent<string[]>) => {
+        const {
+            target: { value },
+        } = event;
+        const tagTexts = typeof value === 'string' ? value.split(',') : value;
+        const toBe = tagTexts.map((tagText: string) => {
+            return diaryTags!.find(tag => tag.text === tagText)!;
+        });
+        setTags(toBe);
+        setCreateDiaryDraft({ ...createDiaryDraft, tagIds: toBe.map(tag => tag.id) });
+    };
+    const onChangeEntry = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const value = event.target.value;
+        setEntry(value);
+        setCreateDiaryDraft({ ...createDiaryDraft, entry: value });
+    };
+
+    useEffect(() => {
+        if (createDiaryDraft?.date === undefined) return;
+        setDate(parse(createDiaryDraft.date, 'yyyy-MM-dd', new Date()));
+    }, [createDiaryDraft?.date]);
+    useEffect(() => {
+        if (createDiaryDraft?.tagIds === undefined) return;
+        if (diaryTags === undefined) return;
+        const cachedTags = createDiaryDraft.tagIds.map(id => diaryTags.find(tag => tag.id === id)).filter(tag => tag !== undefined) as IDiaryTag[];
+        setTags(cachedTags);
+    }, [createDiaryDraft?.tagIds, diaryTags]);
+    useEffect(() => {
+        if (createDiaryDraft?.entry === undefined) return;
+        setEntry(createDiaryDraft.entry);
+    }, [createDiaryDraft?.entry]);
 
     return (
         <Dialog open={true} onClose={onClose} fullWidth>
+            {/* MYMEMO: 復元しましたとかのメッセージをつける */}
+            {/* MYMEMO: キャッシュを消すボタンをつける */}
             <DialogContent>
                 <FormGroup sx={{ mt: 3 }}>
                     <MobileDatePicker label="日付" value={date} onChange={onChangeDate} showDaysOutsideCurrentMonth closeOnSelect sx={{ mb: 1 }} />
@@ -51,19 +88,7 @@ const CreateDiaryDialog = ({ userRelationId, onClose }: CreateDiaryDialogProps) 
                                 label="tags"
                                 multiple
                                 value={tags.map(tag => tag.text)}
-                                onChange={(event: SelectChangeEvent<string[]>) => {
-                                    const {
-                                        target: { value },
-                                    } = event;
-                                    const tagTexts = typeof value === 'string' ? value.split(',') : value;
-                                    setTags(cur =>
-                                        tagTexts.map((tagText: string) => {
-                                            const exists = cur.find(c => c.text === tagText);
-                                            if (exists) return exists;
-                                            return diaryTags.find(tag => tag.text === tagText)!;
-                                        }),
-                                    );
-                                }}
+                                onChange={onChangeTags}
                                 renderValue={selected => (
                                     <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                                         {selected.map(value => (
@@ -80,7 +105,7 @@ const CreateDiaryDialog = ({ userRelationId, onClose }: CreateDiaryDialogProps) 
                             </Select>
                         </FormControl>
                     )}
-                    <TextField value={entry} onChange={event => setEntry(event.target.value)} label="内容" multiline minRows={5} />
+                    <TextField value={entry} onChange={onChangeEntry} label="内容" multiline minRows={5} />
                 </FormGroup>
                 <Button variant="contained" onClick={handleSubmit} sx={{ mt: 2, mb: 2 }}>
                     保存する
