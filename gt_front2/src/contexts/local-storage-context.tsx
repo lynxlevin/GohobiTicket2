df88e0697892d91aab7e852af671d8027e57c798
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-05-07 20:01:33 +0900

    feat (useLocalStorage): add CreateTicketDraft

--- gt_front2/src/contexts/local-storage-context.tsx
+++ gt_front2/src/contexts/local-storage-context.tsx
@@ -1,17 +1,25 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react';
-import { CreateDiaryDraft } from '../hooks/useLocalStorage';
+import { CreateDiaryDraft, CreateTicketDraft } from '../hooks/useLocalStorage';
 
 interface LocalStorageContextType {
+    createTicketDraft?: CreateTicketDraft;
+    setCreateTicketDraft: Dispatch<SetStateAction<CreateTicketDraft | undefined>>;
     createDiaryDraft?: CreateDiaryDraft;
     setCreateDiaryDraft: Dispatch<SetStateAction<CreateDiaryDraft | undefined>>;
 }
 
 export const LocalStorageContext = createContext<LocalStorageContextType>({
+    setCreateTicketDraft: () => {},
     setCreateDiaryDraft: () => {},
 });
 
 export const LocalStorageProvider = ({ children }: { children: ReactNode }) => {
+    const [createTicketDraft, setCreateTicketDraft] = useState<CreateTicketDraft>();
     const [createDiaryDraft, setCreateDiaryDraft] = useState<CreateDiaryDraft>();
 
-    return <LocalStorageContext.Provider value={{ createDiaryDraft, setCreateDiaryDraft }}>{children}</LocalStorageContext.Provider>;
+    return (
+        <LocalStorageContext.Provider value={{ createTicketDraft, setCreateTicketDraft, createDiaryDraft, setCreateDiaryDraft }}>
+            {children}
+        </LocalStorageContext.Provider>
+    );
 };
--- gt_front2/src/hooks/useLocalStorage.tsx
+++ gt_front2/src/hooks/useLocalStorage.tsx
@@ -1,6 +1,11 @@
 import { useContext, useEffect } from 'react';
 import { LocalStorageContext } from '../contexts/local-storage-context';
 
+export interface CreateTicketDraft {
+    giftDate?: string;
+    description?: string;
+}
+
 export interface CreateDiaryDraft {
     date?: string;
     tagIds?: string[];
@@ -8,23 +13,36 @@ export interface CreateDiaryDraft {
 }
 
 const LOCAL_STORAGE_KEYS = {
+    createTicketDraft: 'createTicketDraft',
     createDiaryDraft: 'createDiaryDraft',
 };
 
 const useLocalStorage = () => {
     const ctx = useContext(LocalStorageContext);
 
+    const setCreateTicketDraft = (draft: CreateTicketDraft) => {
+        localStorage.setItem(LOCAL_STORAGE_KEYS.createTicketDraft, JSON.stringify(draft));
+        ctx.setCreateTicketDraft(draft);
+    };
+    const resetCreateTicketDraft = () => {
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.createTicketDraft);
+        ctx.setCreateTicketDraft(undefined);
+    };
+
     const setCreateDiaryDraft = (draft: CreateDiaryDraft) => {
         localStorage.setItem(LOCAL_STORAGE_KEYS.createDiaryDraft, JSON.stringify(draft));
         ctx.setCreateDiaryDraft(draft);
     };
-
     const resetCreateDiaryDraft = () => {
         localStorage.removeItem(LOCAL_STORAGE_KEYS.createDiaryDraft);
         ctx.setCreateDiaryDraft(undefined);
     };
 
     useEffect(() => {
+        if (ctx.createTicketDraft === undefined) {
+            const value = localStorage.getItem(LOCAL_STORAGE_KEYS.createTicketDraft);
+            ctx.setCreateTicketDraft(value === '' || value === null ? undefined : (JSON.parse(value) as CreateTicketDraft));
+        }
         if (ctx.createDiaryDraft === undefined) {
             const value = localStorage.getItem(LOCAL_STORAGE_KEYS.createDiaryDraft);
             ctx.setCreateDiaryDraft(value === '' || value === null ? undefined : (JSON.parse(value) as CreateDiaryDraft));
@@ -33,6 +51,9 @@ const useLocalStorage = () => {
     }, []);
 
     return {
+        createTicketDraft: ctx.createTicketDraft,
+        setCreateTicketDraft,
+        resetCreateTicketDraft,
         createDiaryDraft: ctx.createDiaryDraft,
         setCreateDiaryDraft,
         resetCreateDiaryDraft,
