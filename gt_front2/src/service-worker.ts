4f9c18686a150739821d5a860a64e3c0f391d26d
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-09-08 12:15:34 +0900

    feat: jump to wish page on tapping WishReply notification

--- gt_front2/src/service-worker.ts
+++ gt_front2/src/service-worker.ts
@@ -85,7 +85,11 @@ self.addEventListener('push', event => {
     let url = process.env.PUBLIC_URL;
     switch (payload.message_type) {
         case 'MakeWish':
-            url += `/user_relations/${payload.user_relation_id}/wishes?wish_id=${payload.wish_id}`;
+            url += `/user_relations/${payload.user_relation_id}/wishes?wishId=${payload.wish_id}`;
+            break;
+        case 'WishReply':
+            url += `/user_relations/${payload.user_relation_id}/wishes/${payload.wish_id}`;
+            break;
     }
     const options = {
         body: payload.body ?? '失敗',
