be4eafcb248356d9a1bf652debfc2341baeb7ffe
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-09-12 15:21:14 +0900

    chore: remove mention of slack

--- gt_front2/src/types/user_relation.ts
+++ gt_front2/src/types/user_relation.ts
@@ -3,7 +3,6 @@ export interface IUserRelation {
     related_username: string;
     giving_ticket_img: string | null;
     receiving_ticket_img: string | null;
-    use_slack: boolean;
     first_giving_ticket_date:  string | null;
     first_receiving_ticket_date: string | null;
     first_diary_date: string | null;
